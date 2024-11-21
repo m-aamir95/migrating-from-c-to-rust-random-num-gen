0e4d184d49f88e167e45772fadc40acdf468611a
Aamir <djzaamir@gmail.com>
2024-11-21 15:02:52 +0500

    Fixed a bug, because of which we were not utilizing functions from the library

--- c-code/main.c
+++ c-code/main.c
@@ -1,17 +1,6 @@
 #include "stdio.h"
 #include "vrandom.h"
 
-unsigned long X = 123456789;
-
-void seed_random_int(unsigned long seed){
-    X = seed;
-}
-
-unsigned long get_random_int(void){
-    X = X * 69069 + 362437;
-    return X;
-}
-
 
 int main(void){
 
